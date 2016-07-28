8b0dacbfcc65b5d559f305fe1b2f1bc0c0fd8e66
RobinDNA <robindna@gmail.com>
2016-07-28 23:13:47 +0800

    commented express

--- Server.js
+++ Server.js
@@ -1,7 +1,8 @@
-var express = require('express');
+/*var express = require('express');
 var AV = require('leanengine');
 
 var app = express();
 
 app.use(AV.express());
+*/
 app.listen(process.env.LEANCLOUD_APP_PORT);
