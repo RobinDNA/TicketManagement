089ff6819a1050a7e09340a399d4c1a28c04e769
RobinDNA <robindna@gmail.com>
2016-07-30 10:52:31 +0800

    update server.js

--- server.js
+++ server.js
@@ -4,9 +4,4 @@ var AV = require('leanengine');
 var app = express();
 
 app.use(AV.express());
-
-var express = require('express');
-var app = express();
-
-app.use(AV.express());
 app.listen(process.env.LEANCLOUD_APP_PORT);
