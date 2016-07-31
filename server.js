702d52431c02e9f561e11a563e0f985a4674ba14
RobinDNA <robindna@gmail.com>
2016-07-31 12:06:24 +0800

    Add dependices modules

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ AV.init({
   masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
 });
 
-// 如果不希望使用 masterKey 权限，可以将下面一行删除
+//// 如果不希望使用 masterKey 权限，可以将下面一行删除
 AV.Cloud.useMasterKey();
 
 var app = require('./app');
@@ -20,7 +20,7 @@ app.listen(PORT, function () {
 
   // 注册全局未捕获异常处理器
   process.on('uncaughtException', function(err) {
-    console.error("Caught exception:", err.stack);
+    console.error("Caught   :", err.stack);
   });
   process.on('unhandledRejection', function(reason, p) {
     console.error("Unhandled Rejection at: Promise ", p, " reason: ", reason.stack);
